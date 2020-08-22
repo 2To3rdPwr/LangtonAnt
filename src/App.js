72324459c9c685c51b365bc3a802c598b976f308
JSage <jsage@callbox.com>
2020-08-21 21:04:38 -0400

    Dynamically expand grid

--- src/App.js
+++ src/App.js
@@ -40,6 +40,37 @@ class App extends Component {
     this.setState({ grid: newGrid });
   }
 
+  expandGrid(grid, ant) {
+    console.log(ant)
+    console.log(grid.length+":"+grid[0].length)
+    let rowLength = grid[0].length;
+    let colLength = grid.length
+    if(ant[0] < 0){
+      let newRow = [];
+      for (let i = 0; i < rowLength; i++){
+        newRow.push(new Cell());
+      }
+      grid.unshift(newRow);
+      ant[0] = 0;
+    } else if (ant[0] >= grid.length){
+      let newRow = [];
+      for (let i = 0; i < rowLength; i++){
+        newRow.push(new Cell());
+      }
+      grid.push(newRow);
+    } else if (ant[1] < 0) {
+      for (let i = 0; i < colLength; i++) {
+        grid[i].unshift(new Cell());
+      }
+      ant[1] = 0;
+    } else if (ant[1] >= grid[0].length) {
+      for (let i = 0; i < colLength; i++) {
+        grid[i].push(new Cell());
+      }
+    }
+    return [grid, ant];
+  }
+
   step() {
     var nextGrid = [...this.state.grid];
     
@@ -80,6 +111,13 @@ class App extends Component {
     
     //Next, scrap old ant
     nextGrid[this.state.ant[0]][this.state.ant[1]].putAnt(4);
+
+    console.log(this.state.ant[0]+":"+this.state.ant[1])
+
+    //If ant moves to a non-existant spot, expand grid in that direction.
+    let shifts = this.expandGrid(nextGrid, newAnt);
+    nextGrid = shifts[0];
+    newAnt = shifts[1];
     
     //put new ant on grid
     nextGrid[newAnt[0]][newAnt[1]].putAnt(newAnt[2]);
@@ -87,7 +125,7 @@ class App extends Component {
 
     setTimeout(() => { 
       this.step();
-    }, 250);
+    }, 10);
   }
 }
 
